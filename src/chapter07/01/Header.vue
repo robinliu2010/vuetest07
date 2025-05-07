1aac6712962ad07c75d22c84353c68ca240e205e
robinliu2010 <robinliu2010@163.com>
2025-05-07 14:31:29 +0800

    Initial commit: Add Vue.js project files

---
+++ src/chapter07/01/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="header">
+        <h4>Header</h4>
+        <h4>NavBar</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
---
+++ src/chapter07/01/MainProductList.vue
@@ -0,0 +1,19 @@
+<template>
+    <h4>商品列表</h4>
+    <ul>
+        <li>商品信息1</li>
+        <li>商品信息2</li>
+        <li>商品信息3</li>
+        <li>商品信息4</li>
+        <li>商品信息5</li>
+    </ul>
+
+</template>
+
+<script>
+export default {
+    name: 'MainProductList'
+}
+</script>
+
+
